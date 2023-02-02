5b6bc037f1ad6d455370a608bc048a47db042283
Zach Gallman <zach.ecab@gmail.com>
2023-02-02 18:00:46 -0500

    Added DisplayCard, DisplayList, AnimatedDisplayCard and HomePage

---
+++ src/features/display/DisplayList.js
@@ -0,0 +1,36 @@
+import { useSelector } from 'react-redux';
+import { Col, Row } from 'reactstrap';
+// import DisplayCard from './DisplayCard';
+import { selectFeaturedWine } from '../wine/winesSlice';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
+
+const DisplayList = () => {
+    const items = useSelector((state) => [
+        selectFeaturedWine(state)
+    ]);
+    console.log('display items:', items);
+    return (
+        <Row>
+        {items.map((item, idx) => {
+            const { featuredItem, isLoading, errMsg } = item;
+            if (isLoading) {
+                return <Loading key={idx} />;
+            }
+            if (errMsg) {
+                return <Error errMsg={errMsg} key={idx} />;
+            }
+            return (
+                featuredItem && (
+                    <Col md className='m-1' key={idx}>
+                        <AnimatedDisplayCard item={featuredItem} />
+                    </Col>
+                )
+            );
+        })}
+    </Row>
+    );
+};
+
+export default DisplayList;
