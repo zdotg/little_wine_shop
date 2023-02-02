c85f16514db37a71fbd34a43282fa598b983cd18
Zach Gallman <zach.ecab@gmail.com>
2023-02-02 14:34:23 -0500

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import WineShopLogo from './app/assets/img/shop.png'; 
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color='primary' sticky='top' expand='md'>
+                <Container>
+                    <NavbarBrand href='/'>
+                        <img src={WineShopLogo} alt='Little Wine Shop logo' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
       Little Wine Shop
     </div>
   );
